3c0e0509bffebd1169faa1efad52cdf9c265f005
Elzevir <safilhoelzevirsafilho@gmail.com>
2020-12-24 20:01:58 -0300

    First Commit

---
+++ src/Components/Central.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function() {
+    return (
+        <React.Fragment>
+        <div>
+            <body>
+              <h1>Central</h1>
+                <h2>Eu sou uma aplicação em react!</h2>  
+            </body>
+        </div>
+        </React.Fragment>
+    )
+}
+
---
+++ src/Components/Topo.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function() {
+    return(
+    <div>
+        <header>
+        <h1>Olá, eu sou a navbar!</h1>
+        </header>
+    </div>
+    )
+}
