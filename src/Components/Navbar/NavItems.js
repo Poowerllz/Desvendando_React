a5ef30fef70ebdbee6409ba876cbbbd8fc53d177
Elzevir <safilhoelzevirsafilho@gmail.com>
2020-12-26 11:43:43 -0300

    Inicio a Navbar e Body

---
+++ src/Components/Navbar/NavItems.js
@@ -0,0 +1,22 @@
+export const item = [
+    {
+        title: "Item",
+        url: "#",
+        cName: "Home"
+    },
+    {
+        title: "Item",
+        url: "#",
+        cName: "Menu"
+    },
+    {
+        title: "Item",
+        url: "#",
+        cName: "Meet"
+    },
+    {
+        title: "Item",
+        url: "#",
+        cName: "Visit"
+    }
+]
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Topo from './Components/Topo'
-import Central from './Components/Central'
+
+
+import Topo from './Components/Navbar/Topo'
+import './Components/Navbar/Navbar.css'
+
+import Central from './Components/Body/Central'
+import './Components/Body/Central.css'
+
+
 import Inferior from './Components/Inferior'
 
 import './App.css'
 
+
 ReactDOM.render(
-  <div>
+  <div className="Content">
     <Topo></Topo>
     <Central></Central>
     <Inferior></Inferior>
